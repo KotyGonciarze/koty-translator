5511e90f0c5768e7c41178e5618e71c467fc7e8d
Dawid Nachlik <dnachlik@nocadis.com>
2025-03-11 21:58:57 +0100

    feat: install tailwind

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-
+import "../styles/index.css";
 export const Route = createRootRoute({
   component: RootComponent,
 });
